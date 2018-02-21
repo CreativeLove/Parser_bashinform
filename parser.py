7dab358899619618759aaf7c0c1e51c74b4b393a
Voniqwer <30521750+Voniqwer@users.noreply.github.com>
2018-02-21 21:58:27 +0500

    bashinform bugfix

--- parser.py
+++ parser.py
@@ -1,14 +1,24 @@
-import urllib.request
-from bs4 import BeautifulSoup
+# import urllib.request
+# from bs4 import BeautifulSoup
 
-def get_html(url):
-	response = urllib.request.urlopen(url)
-	return response.read()
+# def get_html(url):
+# 	response = urllib.request.urlopen(url)
+# 	return response.read()
 
-def main():
-	print(get_html('http://www.bashinform.ru/longread/gornolyzhka/'))
-	#print(get_html('https://www.youtube.com/watch?v=3hgkiDAaSQs'))
+# def main():
+# 	print(get_html('http://www.bashinform.ru/news/'))
 
 
-if __name__ == '__main__':
-	main()
+# if __name__ == '__main__':
+# 	main()
+import random
+import requests
+
+agents = [
+'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko)']
+headers = {"User-Agent":random.choice(agents)}
+
+url = "http://bashinform.ru/news/"
+response = requests.get(url,headers=headers)
+print(response.text)
