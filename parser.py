5c93f1e4c36378b41616b85f6c8a009273e6486e
Green_hood <30521750+Voniqwer@users.noreply.github.com>
2018-02-23 13:34:44 +0500

    1

--- parser.py
+++ parser.py
@@ -1,42 +1,42 @@
-#!/usr/bin/env python3
-import random
 import requests
 import csv
 from bs4 import BeautifulSoup
 
-def get_html(url):
-    agents = [
-    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko)']
-    headers = {"User-Agent":random.choice(agents)}
-    response = requests.get(url,headers=headers)
+
+# Получаем адрес, который будем парсить
+# Возвратим все элементы (теги) страницы
+def get_html(url): 
+    agent = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+    response = requests.get(url,headers = agent)
     return response.text
 
-def parse(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    articles = soup.find_all('article')
-    rows = []
-    for article in articles:
-        names = article.find_all('a', 'name')
-        for i in range(len(names)):
-            link = names[i].attrs['href']
-            if (link[0] == '/'):
-                    link = 'http://www.bashinform.ru' + link
-            rows.append((names[i].get_text(), link))
-            print(names[i].get_text())
-            print(link)
-    return rows
 
-def printCSV(rows):
-    f = open("bashinform.csv", "wb")
-    wr = csv.writer(f, delimiter=";")
-    for i in range(len(rows)):
-        wr.writerow({wr[i].first, wr[i].second})
-    f.close()
+
+# Получаем внутренне содержимое страницы
+def parse(html):
+    soup = BeautifulSoup(html, 'html.parser') # Подключаем Html parser
+    articles = soup.find_all('a', 'name') # Находим все теги a с классом name (article)
+
+    for article in articles:
+    	name = article.text    # Содержимое article  
+    	href = article.attrs['href']    # Ссылка на article  
+    	if (href[0:5] == "/news"):
+    		print(name)
+    		print(href)
+    		print("")
+    		data = [name, href]
+    		printCSV(data)
+
+# Выводит в формат CSV
+def printCSV(data):
+    with open("bashinform.csv", "w", newline="") as f:
+    	wr = csv.writer(f, delimiter=";")
+    	wr.writerow(data)
     
 
 def main():
-    printCSV(parse(get_html('http://www.bashinform.ru')))
+    # parse(get_html('http://www.bashinform.ru'))
+    printCSV(["One", "Two", "Three"])
 
 
 if __name__ == '__main__':
